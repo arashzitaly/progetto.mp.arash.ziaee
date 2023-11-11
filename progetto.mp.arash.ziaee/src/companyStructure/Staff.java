93396669e7c760b81cd03a255a8c0aed0cb85030
Arash <arashziaee@yahoo.com>
2023-11-11 12:06:58 +0100

    Staff base implementation

---
+++ progetto.mp.arash.ziaee/src/companyStructure/Staff.java
@@ -0,0 +1,51 @@
+package companyStructure;
+
+import java.util.Objects;
+
+import company.utils.print.StaffPrinter;
+
+public abstract class Staff {
+
+	
+	private String name;
+
+	protected Staff(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Staff other = (Staff) obj;
+		return Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + "name=" + name + "\n";
+	}
+	
+	public abstract void join(String group);
+	
+	public abstract void leave(String group);
+	
+	public abstract double calculateSalary();
+	
+	public abstract void printMember(StaffPrinter printer);
+	
+	
+}
